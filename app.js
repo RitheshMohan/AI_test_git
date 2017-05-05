666d21f867717933fb3a09789e775596a0cfda16
Mohan <rithesh.mohan@accenture.com>
2017-05-05 14:25:56 +1000

    just like that

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
+
 bot.dialog('/', function (session) {
     session.send("Hello World");
-});
+}); 
